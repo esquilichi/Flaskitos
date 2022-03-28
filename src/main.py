1b5db4460bb71a4536f7fde8f48d72516df5a2ea
pyx1 <74534957+pyx1@users.noreply.github.com>
2022-03-28 12:06:27 +0200

    Restructuracion 3/4
    Casi listo

--- src/main.py
+++ src/main.py
@@ -5,7 +5,14 @@ from Ejercicio4 import ejercicio4
 
 if __name__ == '__main__':
     df_usuarios, df_legal = generar_dataframes()
-    ejercicio2(df_usuarios)
-    ejercicio3(df_usuarios)
-    ejercicio3_v2(df_usuarios)
-    ejercicio4(df_usuarios, df_legal)
+    with open('../respuestas.txt', 'w') as sols:
+        sols.write("Ismael Gomez, Adriano Campos, Alejandro Gallego\nGrupo R\nPractica 1\nEstas son las respuestas obtenidas para cada uno de los apartados de la practica:\n")
+        sols.write("------Ejercicio 2------\n\n")
+        ejercicio2(df_usuarios, sols)
+        sols.write("----Fin del ejercicio 2----\n")
+        sols.write("------Ejercicio 3------\n\n")
+        ejercicio3(df_usuarios, sols)
+        ejercicio3_v2(df_usuarios, sols)
+        sols.write("----Fin del ejercicio 3----\n")
+        ejercicio4(df_usuarios, df_legal)
+        sols.close()
