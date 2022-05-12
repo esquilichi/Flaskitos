2a391e31c48cac4101dd3e655a9d9008d56b4220
Ismael Esquilichi <67276772+esquilichi@users.noreply.github.com>
2022-05-12 19:14:51 +0200

    Cambios

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import plotly.graph_objects as go
 
 app = Flask(__name__)
 
-def get_driver(onion=True):
+def get_driver():
     # Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0
     profile = webdriver.FirefoxProfile()
     webdriver_options = FirefoxOptions()
