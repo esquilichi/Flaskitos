bf627b861261be995862bd1c73b988153ff1487f
Ismael Esquilichi <67276772+esquilichi@users.noreply.github.com>
2022-05-12 18:30:28 +0200

    Scrapping casi listo

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import pandas as pd
 from flask import Flask, render_template, redirect, url_for, request, send_file, Response
 from Entrega1.src.database import *
 from Entrega1.src.Ejercicio4 import *
+import requests
+import plotly
+import plotly.graph_objects as go
+
+
 app = Flask(__name__)
 
 
@@ -11,13 +16,14 @@ def hello_world():
 
 @app.route('/dashboard')
 def dashboard():
+    l = ejercicio4()
     x = request.args.get('n', default=10, type=int)
     c, conn = connect_db("Entrega1/database/database.db")
     df1 = pd.read_sql_query("select * from users", conn)
     df1 = porcentaje_peligro(df1)
     df1 = usuarios_criticos(df1).head(x)
     top_users_plot(df1)
-    return render_template('pages/dashboard.html')
+    return render_template('pages/dashboard.html', item=l)
 
 @app.route('/graphics/<id>', methods=['GET'])
 def get_graphic(id):
@@ -38,6 +44,15 @@ def get_datajotason(id):
     except Exception as e:
         return str(e)
 
+
+def ejercicio4():
+    r = requests.get("https://cve.circl.lu/api/last")
+    jotason = json.loads(r.text)
+    l = []
+    for i in range(10):
+        temp = jotason[i]
+        l.append(temp)
+    return l
 
 if __name__ == '__main__':
     app.run(debug=True)
